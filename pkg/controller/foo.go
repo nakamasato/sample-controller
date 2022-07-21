8a9d07fa8eaddb7de2dd625337a1c30031cb8e8c
Masato Naka <masatonaka1989@gmail.com>
2022-07-22 07:21:58 +0900

    5.1. Create Controller

---
+++ pkg/controller/foo.go
@@ -0,0 +1,72 @@
+package controller
+
+import (
+	"log"
+	"time"
+
+	clientset "github.com/nakamasato/sample-controller/pkg/client/clientset/versioned"
+	informers "github.com/nakamasato/sample-controller/pkg/client/informers/externalversions/example.com/v1alpha1"
+	listers "github.com/nakamasato/sample-controller/pkg/client/listers/example.com/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+type Controller struct {
+	// sampleclientset is a clientset for our own API group
+	sampleclientset clientset.Interface
+
+	foosLister listers.FooLister    // lister for foo
+	foosSynced cache.InformerSynced // cache is synced for foo
+
+	// queue
+	workqueue workqueue.RateLimitingInterface
+}
+
+func NewController(sampleclientset clientset.Interface, fooInformer informers.FooInformer) *Controller {
+	controller := &Controller{
+		sampleclientset: sampleclientset,
+		foosSynced:      fooInformer.Informer().HasSynced,
+		foosLister:      fooInformer.Lister(),
+		workqueue:       workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "foo"),
+	}
+
+	fooInformer.Informer().AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    controller.handleAdd,
+			DeleteFunc: controller.handleDelete,
+		},
+	)
+
+	return controller
+}
+
+func (c *Controller) Run(stopCh <-chan struct{}) error {
+	if ok := cache.WaitForCacheSync(stopCh, c.foosSynced); !ok {
+		log.Printf("cache is not synced")
+	}
+
+	go wait.Until(c.worker, time.Second, stopCh)
+
+	<-stopCh
+	return nil
+}
+
+func (c *Controller) worker() {
+	c.processNextItem()
+}
+
+func (c *Controller) processNextItem() bool {
+	return true
+}
+
+func (c *Controller) handleAdd(obj interface{}) {
+	log.Println("handleAdd was called")
+	c.workqueue.Add(obj)
+}
+
+func (c *Controller) handleDelete(obj interface{}) {
+	log.Println("handleDelete was called")
+	c.workqueue.Add(obj)
+}
