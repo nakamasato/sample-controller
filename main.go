ddf6207da991010b63dc39f40c4bdb067262bc45
Masato Naka <masatonaka1989@gmail.com>
2022-07-25 09:43:47 +0900

    tentative new version

--- main.go
+++ main.go
@@ -1,43 +1,58 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "path/filepath"
+	"flag"
+	"path/filepath"
+	"time"
 
-    "k8s.io/client-go/tools/clientcmd"
-    "k8s.io/client-go/util/homedir"
-    "k8s.io/klog/v2"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog/v2"
 
-    clientset "github.com/nakamasato/sample-controller/pkg/generated/clientset/versioned"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "github.com/nakamasato/sample-controller/pkg/generated/clientset/versioned"
+	informers "github.com/nakamasato/sample-controller/pkg/generated/informers/externalversions"
 )
 
 func main() {
-    klog.InitFlags(nil)
-    var kubeconfig *string
+	klog.InitFlags(nil)
+	var kubeconfig *string
 
-    if home := homedir.HomeDir(); home != "" {
-        kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional)")
-    } else {
-        kubeconfig = flag.String("kubeconfig", "", "absolute path to kubeconfig file")
-    }
-    flag.Parse()
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional)")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to kubeconfig file")
+	}
+	flag.Parse()
 
-    config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-    if err != nil {
-        klog.Fatalf("Error building kubeconfig: %s", err.Error())
-    }
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
 
-    exampleClientset, err := clientset.NewForConfig(config)
-    if err != nil {
-        klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
-    }
-    klog.Info(exampleClientset)
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Errorf("getting kubernetes client set %s\n", err.Error())
+	}
 
-    foos, err := exampleClientset.ExampleV1alpha1().Foos("").List(context.Background(), metav1.ListOptions{})
-    if err != nil {
-        klog.Fatalf("listing foos %s %s", err.Error())
-    }
-    klog.Infof("length of foos is %d", len(foos.Items))
+	exampleClientset, err := clientset.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	exampleInformerFactory := informers.NewSharedInformerFactory(exampleClientset, time.Second*30)
+	stopCh := make(chan struct{})
+	controller := NewController(
+		kubeClient,
+		exampleClientset,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		exampleInformerFactory.Example().V1alpha1().Foos(),
+	)
+	kubeInformerFactory.Start(stopCh)
+	exampleInformerFactory.Start(stopCh)
+	if err = controller.Run(stopCh); err != nil {
+		klog.Fatalf("error occurred when running controller %s\n", err.Error())
+	}
 }
