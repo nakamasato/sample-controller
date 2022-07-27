ec7d3bbfd28d2a7e287bc1bbe2aa78134e0c2933
Naka Masato <masatonaka1989@gmail.com>
2022-07-28 08:57:44 +0900

    chore: update tools/publish-release.sh (#29)

--- tools/publish-release.sh
+++ tools/publish-release.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+RELEASE_MD=release.md
+
 latest_version=$(gh release list -L 1 | cut -f1)
 patch_version=$(echo $latest_version | sed 's/v[0-9]*\.[0-9]*\.\([0-9]*\)/\1/')
 new_patch_version=$((patch_version+1))
@@ -10,8 +12,9 @@ echo "latest_version: $latest_version, new_version: $new_version"
 git tag -a $new_version -m "release"
 git push origin --tag
 gh release create $new_version --generate-notes
-gh release view $new_version --json body -q .body > release.md
+gh release view $new_version --json body -q .body > $RELEASE_MD
 
-echo "## Contents" >> release.md
-for f in docs/content/docs/*/index.md; do grep -e '## \[[0-9]' $f | sed 's/##/-/g';done >> release.md
-gh release edit $new_version --notes-file release.md
+echo "## Contents" >> $RELEASE_MD
+for f in docs/content/docs/*/index.md; do grep -e '## \[[0-9]' $f | sed 's/##/-/g';done >> $RELEASE_MD
+gh release edit $new_version --notes-file $RELEASE_MD
+rm $RELEASE_MD
