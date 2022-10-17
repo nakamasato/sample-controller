9f1b9a9d4536178c00c935d7e49fcb7f66cdc437
Masato Naka <masatonaka1989@gmail.com>
2022-10-17 10:13:22 +0900

    5.3. Create/Delete Deployment for Foo resource

--- controller.go
+++ controller.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	samplev1alpha1 "github.com/nakamasato/sample-controller/pkg/apis/example.com/v1alpha1"
 	clientset "github.com/nakamasato/sample-controller/pkg/generated/clientset/versioned"
 	informers "github.com/nakamasato/sample-controller/pkg/generated/informers/externalversions/example.com/v1alpha1"
 	listers "github.com/nakamasato/sample-controller/pkg/generated/listers/example.com/v1alpha1"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
+	"k8s.io/client-go/kubernetes"
+	appslisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 )
 
 type Controller struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
 	// sampleclientset is a clientset for our own API group
 	sampleclientset clientset.Interface
+
+	deploymentsLister appslisters.DeploymentLister
+	deploymentsSynced cache.InformerSynced
 
 	foosLister listers.FooLister
 	foosSynced cache.InformerSynced // cache is synced for foo
@@ -25,18 +39,24 @@ type Controller struct {
 	workqueue workqueue.RateLimitingInterface
 }
 
-func NewController(sampleclientset clientset.Interface, fooInformer informers.FooInformer) *Controller {
+func NewController(
+	kubeclientset kubernetes.Interface,
+	sampleclientset clientset.Interface,
+	deploymentInformer appsinformers.DeploymentInformer,
+	fooInformer informers.FooInformer) *Controller {
 	controller := &Controller{
-		sampleclientset: sampleclientset,
-		foosSynced:      fooInformer.Informer().HasSynced,
-		foosLister:      fooInformer.Lister(),
-		workqueue:       workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "foo"),
+		kubeclientset:     kubeclientset,
+		sampleclientset:   sampleclientset,
+		deploymentsLister: deploymentInformer.Lister(),
+		deploymentsSynced: deploymentInformer.Informer().HasSynced,
+		foosLister:        fooInformer.Lister(),
+		foosSynced:        fooInformer.Informer().HasSynced,
+		workqueue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "foo"),
 	}
 
 	fooInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    controller.handleAdd,
-			DeleteFunc: controller.handleDelete,
+			AddFunc: controller.handleAdd,
 		},
 	)
 	return controller
@@ -81,19 +101,11 @@ func (c *Controller) processNextWorkItem() bool {
 			return nil
 		}
 
-		ns, name, err := cache.SplitMetaNamespaceKey(key)
-		if err != nil {
-			klog.Errorf("failed to split key into namespace and name %s", err.Error())
-			return err
+		if err := c.syncHandler(key); err != nil {
+			// Put the item back on the workqueue to handle any transient errors.
+			c.workqueue.AddRateLimited(key)
+			return fmt.Errorf("error syncing '%s': %s, requeuing", key, err.Error())
 		}
-
-		// temporary main logic
-		foo, err := c.foosLister.Foos(ns).Get(name)
-		if err != nil {
-			klog.Errorf("failed to get foo resource from lister %s", err.Error())
-			return err
-		}
-		klog.Infof("Got foo %+v", foo.Spec)
 
 		// Forget the queue item as it's successfully processed and
 		// the item will not be requeued.
@@ -114,11 +126,6 @@ func (c *Controller) handleAdd(obj interface{}) {
 	c.enqueueFoo(obj)
 }
 
-func (c *Controller) handleDelete(obj interface{}) {
-	klog.Info("handleDelete is called")
-	c.enqueueFoo(obj)
-}
-
 // enqueueFoo takes a Foo resource and converts it into a namespace/name
 // string which is then put onto the work queue. This method should *not* be
 // passed resources of any type other than Foo.
@@ -130,4 +137,72 @@ func (c *Controller) enqueueFoo(obj interface{}) {
 		return
 	}
 	c.workqueue.Add(key)
+}
+
+func (c *Controller) syncHandler(key string) error {
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		klog.Errorf("failed to split key into namespace and name %s", err.Error())
+		return err
+	}
+
+	foo, err := c.foosLister.Foos(ns).Get(name)
+	if err != nil {
+		klog.Errorf("failed to get foo resource from lister %s", err.Error())
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	deploymentName := foo.Spec.DeploymentName
+	if deploymentName == "" {
+		klog.Errorf("deploymentName must be specified %s", key)
+		return nil
+	}
+	deployment, err := c.deploymentsLister.Deployments(foo.Namespace).Get(deploymentName)
+	if errors.IsNotFound(err) {
+		deployment, err = c.kubeclientset.AppsV1().Deployments(foo.Namespace).Create(context.TODO(), newDeployment(foo), metav1.CreateOptions{})
+	}
+
+	if err != nil {
+		return err
+	}
+
+	klog.Infof("deployment %s is valid", deployment.Name)
+
+	return nil
+}
+
+func newDeployment(foo *samplev1alpha1.Foo) *appsv1.Deployment {
+	labels := map[string]string{
+		"app":        "nginx",
+		"controller": foo.Name,
+	}
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            foo.Spec.DeploymentName,
+			Namespace:       foo.Namespace,
+			OwnerReferences: []metav1.OwnerReference{*metav1.NewControllerRef(foo, samplev1alpha1.SchemeGroupVersion.WithKind("Foo"))},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: foo.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "nginx",
+							Image: "nginx:latest",
+						},
+					},
+				},
+			},
+		},
+	}
 }
